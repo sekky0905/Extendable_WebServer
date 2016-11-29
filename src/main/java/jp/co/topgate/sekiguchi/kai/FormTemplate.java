480b03f63f8cb8a58d2db153429e0a2d7279a6f9
SekiguchiKai <kai.sekiguchi@topgate.co.jp>
2016-11-29 13:48:54 +0900

    表示のみStyle適用、うまく動かない

--- src/main/java/jp/co/topgate/sekiguchi/kai/FormTemplate.java
+++ src/main/java/jp/co/topgate/sekiguchi/kai/FormTemplate.java
@@ -12,25 +12,37 @@ public class FormTemplate implements Template {
      * @return HTMLテンプレートをbyte[]にしたもの
      */
     public byte[] writeHTML() {
-
         StringBuilder stringBuilder = new StringBuilder();
+
         stringBuilder.append("<!DOCTYPE html>");
-        stringBuilder.append("<html lang=\"en\">");
+        stringBuilder.append("<html lang=\"ja\">");
         stringBuilder.append("<head>");
         stringBuilder.append("<meta charset=\"UTF-8\">");
-        stringBuilder.append("<title>Document</title>");
+        stringBuilder.append("<title>掲示板</title>");
+        stringBuilder.append("<style>");
+        stringBuilder.append("table {");
+        stringBuilder.append("border-collapse: collapse;");
+        stringBuilder.append(" }");
+        stringBuilder.append("td,");
+        stringBuilder.append("th {");
+        stringBuilder.append("border: solid 1px;");
+        stringBuilder.append("padding: 0.5em;");
+        stringBuilder.append("</style>");
         stringBuilder.append("</head>");
         stringBuilder.append("<body>");
+
         stringBuilder.append("<form action=\"/program/board/registered\" method=\"post\" accept-charset=\"UTF-8\">");
-        stringBuilder.append("<p>");
-        stringBuilder.append("名前：<input type=\"text\" name=\"userName\" size=\"40\">");
-        stringBuilder.append("</p>");
-        stringBuilder.append("<p>");
-        stringBuilder.append("<textarea name=\"comment\" rows=\"4\" cols=\"40\">ここにコメントを記入してください</textarea><br>");
-        stringBuilder.append("</p>");
-        stringBuilder.append("<p>");
-        stringBuilder.append("<input type=\"submit\" value=\"送信する\">");
-        stringBuilder.append("</p>");
+        stringBuilder.append("<table>");
+        stringBuilder.append("<tr>");
+        stringBuilder.append("<th>ユーザーネーム:</th>");
+        stringBuilder.append("<td><input type=\"text \" name=\"userName \" size=\"40 \"></td>");
+        stringBuilder.append("</tr>");
+        stringBuilder.append("<tr>");
+        stringBuilder.append("<th>コメント</th>");
+        stringBuilder.append("<td><textarea name=\"comment \" rows=\"4 \" cols=\"40 \">ここにコメントを記入してください</textarea><br></td>");
+        stringBuilder.append("</tr>");
+        stringBuilder.append("</table>");
+        stringBuilder.append("<p><input type=\"submit\" value=\"送信する\"></p>");
         stringBuilder.append("</form>");
         stringBuilder.append("</body>");
         stringBuilder.append("</html>");
--- src/main/java/jp/co/topgate/sekiguchi/kai/ResultTemplate.java
+++ src/main/java/jp/co/topgate/sekiguchi/kai/ResultTemplate.java
@@ -14,20 +14,43 @@ public class ResultTemplate implements Template {
 
 
         stringBuilder.append("<!DOCTYPE html>");
-        stringBuilder.append("<html lang=\"en\">");
+        stringBuilder.append("<html lang=\"ja\">");
         stringBuilder.append("<head>");
         stringBuilder.append("<meta charset=\"UTF-8\">");
-        stringBuilder.append("<title>Document</title>");
+        stringBuilder.append("<title>掲示板</title>");
+        stringBuilder.append("<style>");
+        stringBuilder.append("table {");
+        stringBuilder.append("border-collapse: collapse;");
+        stringBuilder.append(" }");
+        stringBuilder.append("td,");
+        stringBuilder.append("th {");
+        stringBuilder.append("border: solid 1px;");
+        stringBuilder.append("padding: 0.5em;");
+        stringBuilder.append("</style>");
         stringBuilder.append("</head>");
         stringBuilder.append("<body>");
 
         for (int i = 0; i < MessageStorage.countModel(); i++) {
             Message message = (Message) MessageStorage.getModelList(i);
 
-            stringBuilder.append("<p>投稿日時:" + message.getAtTime() + "</p>");
-            stringBuilder.append("<p>名前:" + message.getUserName() + "</p>");
-            stringBuilder.append("<p>コメント</p>");
-            stringBuilder.append("<p>" + message.getComment() + "</p>");
+            stringBuilder.append("<table>");
+            stringBuilder.append("<tr>");
+            stringBuilder.append("<th>投稿日時:</th>");
+            stringBuilder.append("<td>" + message.getAtTime() + "</td>");
+            stringBuilder.append("</tr>");
+
+            stringBuilder.append("<tr>");
+            stringBuilder.append("<th>ユーザーネーム:</th>");
+            stringBuilder.append("<td>" + message.getUserName() + "</td>");
+            stringBuilder.append("</tr>");
+
+            stringBuilder.append("<tr>");
+            stringBuilder.append("<th>コメント</th>");
+            stringBuilder.append("<td>" + message.getComment() + "</td>");
+            stringBuilder.append("</tr>");
+            stringBuilder.append("</table>");
+
+
             stringBuilder.append("<form action=\"/program/board/registered/afterDelete\" method=\"post\" accept-charset=\"UTF-8\">");
             stringBuilder.append("<p>");
             stringBuilder.append("<input type=\"hidden\" name =\"delete\" value=" + i + ">");
@@ -40,25 +63,33 @@ public class ResultTemplate implements Template {
 
 
         stringBuilder.append("<form action=\"/program/board/registered\" method=\"post\" accept-charset=\"UTF-8\">");
-        stringBuilder.append("<p>");
-        stringBuilder.append("名前：<input type=\"text\" name=\"userName\" size=\"40\">");
-        stringBuilder.append("</p>");
-        stringBuilder.append("<p>");
-        stringBuilder.append("<textarea name=\"comment\" rows=\"4\" cols=\"40\">ここにコメントを記入してください</textarea><br>");
-        stringBuilder.append("</p>");
-        stringBuilder.append("<p>");
-        stringBuilder.append("<input type=\"submit\" value=\"送信する\">");
-        stringBuilder.append("</p>");
+        stringBuilder.append("<table>");
+        stringBuilder.append("<tr>");
+        stringBuilder.append("<th>ユーザーネーム:</th>");
+        stringBuilder.append("<td><input type=\"text \" name=\"userName \" size=\"40 \"></td>");
+        stringBuilder.append("</tr>");
+        stringBuilder.append("<tr>");
+        stringBuilder.append("<th>コメント</th>");
+        stringBuilder.append("<td><textarea name=\"comment \" rows=\"4 \" cols=\"40 \">ここにコメントを記入してください</textarea><br></td>");
+        stringBuilder.append("</tr>");
+        stringBuilder.append("</table>");
+        stringBuilder.append("<p><input type=\"submit\" value=\"送信する\"></p>");
         stringBuilder.append("</form>");
-        stringBuilder.append("<p>");
-        stringBuilder.append("<form action=\"/program/board/registered/search\" method=\"post\" accept-charset=\"UTF-8\">");
+        stringBuilder.append("</body>");
+        stringBuilder.append("</html>");
+
+
         stringBuilder.append("指定したユーザーの書き込みのみ表示させることができます。<br>");
         stringBuilder.append("下記で、検索したいユーザー名を指定してください<br>");
-        stringBuilder.append("ユーザー名:<input type=\"text\" name=\"searchName\">");
-        stringBuilder.append("</p>");
-        stringBuilder.append("<p>");
+
+        stringBuilder.append("<form action=\"/program/board/registered/search\" method=\"post\" accept-charset=\"UTF-8\">");
+        stringBuilder.append("<table>");
+        stringBuilder.append("<tr>");
+        stringBuilder.append("<th>ユーザーネーム:</th>");
+        stringBuilder.append("<td><input type=\"text\" name=\"searchName\"></td>");
+        stringBuilder.append("</tr>");
+        stringBuilder.append("</table>");
         stringBuilder.append("<input type=\"submit\" value=\"検索する\">");
-        stringBuilder.append("</p>");
         stringBuilder.append("</form>");
         stringBuilder.append("</body>");
         stringBuilder.append("</html>");

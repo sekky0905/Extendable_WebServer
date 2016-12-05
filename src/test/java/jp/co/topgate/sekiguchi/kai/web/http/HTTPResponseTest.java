38a8be2831ce572c506d715ea43e64830ac6427e
SekiguchiKai <kai.sekiguchi@topgate.co.jp>
2016-12-05 14:59:13 +0900

    HTTPResponseTestクラスのSetResponseHeader()メソッドを修正

--- src/test/java/jp/co/topgate/sekiguchi/kai/web/http/HTTPResponseTest.java
+++ src/test/java/jp/co/topgate/sekiguchi/kai/web/http/HTTPResponseTest.java
@@ -7,7 +7,9 @@ import static org.junit.Assert.assertThat;
 import java.io.*;
 import java.nio.file.Files;
 
+import jp.co.topgate.sekiguchi.kai.web.handler.ResisterMessageHandler;
 import jp.co.topgate.sekiguchi.kai.web.http.HTTPResponse;
+import jp.co.topgate.sekiguchi.kai.web.util.ResponseHeaderMaker;
 import jp.co.topgate.sekiguchi.kai.web.webServer.StaticFileHandler;
 import org.junit.Test;
 
@@ -18,61 +20,61 @@ import org.junit.Test;
  */
 public class HTTPResponseTest {
 
-//    /**
-//     * StatusLineメソッドをテストするクラス¥
-//     */
-//    @Test
-//    public void SetStatusLine() {
-//
-//        OutputStream outputStream = new ByteArrayOutputStream();
-//        HTTPResponse httpResponse = new HTTPResponse(outputStream);
-//
-//        httpResponse.setStatusLine("HTTP/1.1 200 OK");
-//        String statusLine = httpResponse.getStatusLine();
-//
-//        assertThat(statusLine, is("HTTP/1.1 200 OK"));
-//
-//        OutputStream outputStream2 = new ByteArrayOutputStream();
-//        HTTPResponse httpResponse2 = new HTTPResponse(outputStream2);
-//
-//        httpResponse2.setStatusLine("HTTP/1.1 404 Not Found");
-//        String statusLine2 = httpResponse2.getStatusLine();
-//
-//        assertThat(statusLine2, is("HTTP/1.1 404 Not Found"));
-//
-//    }
-//
-//    /**
-//     * SetResponseHeaderメソッドをテストするクラス
-//     */
-//    @Test
-//    public void SetResponseHeader() {
-//
-//        String fileExtension1 = "html";
-//        String fileExtension2 = "css";
-//        String fileExtension3 = "js";
-//        String fileExtension4 = "jpeg";
-//        String fileExtension5 = "png";
-//        String fileExtension6 = "gif";
-//
-//        String extTextArray[] = {fileExtension1, fileExtension2, fileExtension3};
-//        String extImageArray[] = {fileExtension4, fileExtension5, fileExtension6};
-//
-//        for (String ext : extTextArray) {
-//            OutputStream outputStream = new ByteArrayOutputStream();
-//            HTTPResponse httpResponse = new HTTPResponse(outputStream);
-//            httpResponse.setResponseHeader(ext);
-//            assertThat(httpResponse.getResponseHeader(), is("Content-Type: text/" + ext));
-//        }
-//
-//        for (String ext : extImageArray) {
-//            OutputStream outputStream = new ByteArrayOutputStream();
-//            HTTPResponse httpResponse = new HTTPResponse(outputStream);
-//            httpResponse.setResponseHeader(ext);
-//            assertThat(httpResponse.getResponseHeader(), is("Content-Type: image/" + ext));
-//        }
-//
-//    }
+    /**
+     * StatusLineメソッドをテストするクラス¥
+     */
+    @Test
+    public void SetStatusLine() {
+
+        OutputStream outputStream = new ByteArrayOutputStream();
+        HTTPResponse httpResponse = new HTTPResponse(outputStream);
+
+        httpResponse.setStatusLine("HTTP/1.1 200 OK");
+        String statusLine = httpResponse.getStatusLine();
+
+        assertThat(statusLine, is("HTTP/1.1 200 OK"));
+
+        OutputStream outputStream2 = new ByteArrayOutputStream();
+        HTTPResponse httpResponse2 = new HTTPResponse(outputStream2);
+
+        httpResponse2.setStatusLine("HTTP/1.1 404 Not Found");
+        String statusLine2 = httpResponse2.getStatusLine();
+
+        assertThat(statusLine2, is("HTTP/1.1 404 Not Found"));
+
+    }
+
+    /**
+     * SetResponseHeaderメソッドをテストするクラス
+     */
+    @Test
+    public void SetResponseHeader() {
+
+        String fileExtension1 = "html";
+        String fileExtension2 = "css";
+        String fileExtension3 = "js";
+        String fileExtension4 = "jpeg";
+        String fileExtension5 = "png";
+        String fileExtension6 = "gif";
+
+        String extTextArray[] = {fileExtension1, fileExtension2, fileExtension3};
+        String extImageArray[] = {fileExtension4, fileExtension5, fileExtension6};
+
+        for (String ext : extTextArray) {
+            OutputStream outputStream = new ByteArrayOutputStream();
+            HTTPResponse httpResponse = new HTTPResponse(outputStream);
+            httpResponse.setResponseHeader(ResponseHeaderMaker.makeContentType(ext));
+            assertThat(httpResponse.getResponseHeader(), is("Content-Type: text/" + ext + "\n"));
+        }
+
+        for (String ext : extImageArray) {
+            OutputStream outputStream = new ByteArrayOutputStream();
+            HTTPResponse httpResponse = new HTTPResponse(outputStream);
+            httpResponse.setResponseHeader(ResponseHeaderMaker.makeContentType(ext));
+            assertThat(httpResponse.getResponseHeader(), is("Content-Type: image/" + ext + "\n"));
+        }
+
+    }
 //
 //    /**
 //     * SetResponseHeaderメソッドをテストするクラス¥

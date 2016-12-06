afcc7883ba4681f5510aeb4c58624d5aee6b53c4
SekiguchiKai <kai.sekiguchi@topgate.co.jp>
2016-12-06 12:52:37 +0900

    XSS対策のためのXSSMeasureクラスとそのテストクラスであるXSSMeasureTestクラスを作成

---
+++ src/test/java/jp/co/topgate/sekiguchi/kai/web/util/XSSMeasureTest.java
@@ -0,0 +1,29 @@
+package jp.co.topgate.sekiguchi.kai.web.util;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+/**
+ * Created by sekiguchikai on 2016/12/06.
+ */
+public class XSSMeasureTest {
+
+    @Test
+    public void sanitize() {
+
+        String specialCharArray[] = {"&", "<", ">", "\"", "'", "/"};
+        String expectedCharArray[] = {"&amp;", "&lt;", "&gt;", "&quot;", "&#x27;", "&#x2F;"};
+
+        for (int i = 0; i < specialCharArray.length; i++) {
+            assertThat(XSSMeasure.sanitize(specialCharArray[i]), is(expectedCharArray[i]));
+
+        }
+    }
+
+
+}
+
+

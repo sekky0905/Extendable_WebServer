bf1daf662f20447e6da92ad44190742f0df9d883
SekiguchiKai <kai.sekiguchi@topgate.co.jp>
2016-12-08 15:35:40 +0900

    Sessionクラスのsanitizeメソッドの引数の名前を変更

--- src/main/java/jp/co/topgate/sekiguchi/kai/web/util/Session.java
+++ src/main/java/jp/co/topgate/sekiguchi/kai/web/util/Session.java
@@ -38,10 +38,10 @@ public class Session {
     /**
      * 引数で渡されたtokenが発行したtokenと同じものであるか確認するためのメソッド
      *
-     * @param acceptToken formでHTMLから受け取ったtoken
+     * @param target formでHTMLから受け取ったtoken
      * @return 受け取ったtokenが、以前発行したtokenと同じものであるかの真偽値
      */
-    public static boolean confirmToken(String acceptToken) {
-        return acceptToken.equals(Session.token);
+    public static boolean confirmToken(String target) {
+        return target.equals(Session.token);
     }
 }
